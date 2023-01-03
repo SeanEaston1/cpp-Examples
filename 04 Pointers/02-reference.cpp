4e3d872ef81d6211f6d1b7251c3ab332ed1ae71e
Tanay Raj Srivastava <codebytanay@gmail.com>
2023-01-03 22:37:36 +0530

    Add files via upload

---
+++ 04 Pointers/02-reference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x=10;
+    
+    int &y=x; //Here y is reference or alias or nickname of x//
+    
+    cout<<"X is "<<x<<endl;
+    cout<<"Y is "<<y<<endl;
+}
